3f2f0d4d8b73cadc121a230b2b43655f8a0102f7
Nirav Soni <soninirav910@gmail.com>
2023-02-11 17:28:51 +0530

    Further updates

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -49,21 +49,27 @@ class ContactIndex extends React.Component{
                             <RemoveAllContact/>
                         </div>
                         <div className="row py-2">
+                        <div className="col-8 offset-2 row">
                             <AddContact/>
+                            </div>
                         </div>
                         <div className="row py-2">
+                        <div className="col-8 offset-2 row">
                             <FavoriteContacts 
                             contacts={
                                 this.state.contactList.filter
                                 ((u) => u.isFavorite == true)}
                             />
                         </div>
+                        </div>
                         <div className="row py-2">
+                        <div className="col-8 offset-2 row">
                             <GeneralContacts
                             contacts={
                                 this.state.contactList.filter
                                 ((u) => u.isFavorite == false)}
                                 />
+                        </div>
                         </div>
                     </div>
                 </div>
