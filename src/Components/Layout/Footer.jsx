8436456b609a09e2a6402130d4f5a6746303a9a7
Nirav Soni <soninirav910@gmail.com>
2023-02-11 15:28:28 +0530

    Initial structure

---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return(
+    <div style={{
+        color: "gray",
+        marginTop: "10px",
+        borderTop: "1px solid #555",
+        textAlign: "center",
+    }}>
+        
+    </div>);
+}
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import Header from './Header';
+import Header from './Components/Layout/Header';
+import Footer  from './Components/Layout/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div>
