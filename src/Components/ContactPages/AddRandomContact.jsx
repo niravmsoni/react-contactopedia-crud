5b6382021027694bac90b94e85647fb9b20cf06d
Nirav Soni <soninirav910@gmail.com>
2023-02-14 20:15:04 +0530

    Added axios call

--- src/Components/ContactPages/AddRandomContact.jsx
+++ src/Components/ContactPages/AddRandomContact.jsx
@@ -1,7 +1,14 @@
+import { getRandomUser } from "../../Utility/api";
+
+const GetRandomContact = async () => {
+const apiResponse = await getRandomUser();
+console.log(apiResponse);
+};
+
 const AddRandomContact = () => {
     return(
     <div>
-        <button className="btn btn-success form-control">Add Random Contact</button>
+        <button className="btn btn-success form-control" onClick={() => GetRandomContact()}>Add Random Contact</button>
     </div>);
 }
 export default AddRandomContact;
