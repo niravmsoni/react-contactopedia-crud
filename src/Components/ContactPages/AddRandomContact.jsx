1c99962ba70b536476f8ce064842de000d9a1827
Nirav Soni <soninirav910@gmail.com>
2023-02-14 20:22:14 +0530

    Add random contact implementation completed

--- src/Components/ContactPages/AddRandomContact.jsx
+++ src/Components/ContactPages/AddRandomContact.jsx
@@ -1,14 +1,20 @@
 import { getRandomUser } from "../../Utility/api";
 
-const GetRandomContact = async () => {
+const GetRandomContact = async (props) => {
 const apiResponse = await getRandomUser();
 console.log(apiResponse);
+
+return props.handleAddRandomContact({
+    name: apiResponse.data.first_name + " " + apiResponse.data.last_name,
+    email: apiResponse.data.email,
+    phone: apiResponse.data.phone_number,
+    });
 };
 
-const AddRandomContact = () => {
+const AddRandomContact = (props) => {
     return(
     <div>
-        <button className="btn btn-success form-control" onClick={() => GetRandomContact()}>Add Random Contact</button>
+        <button className="btn btn-success form-control" onClick={() => GetRandomContact(props)}>Add Random Contact</button>
     </div>);
 }
 export default AddRandomContact;
